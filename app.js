0281820d5967059295d724a92d2de7bf81eeb16c
Wesley Millan Silva <millan_wesley@hotmail.com>
2024-09-07 13:17:14 -0300

    Configurando a pesquisa

--- app.js
+++ app.js
@@ -2,20 +2,40 @@ function pesquisar() {
     // Seleciona a seção HTML com o ID "resultados-pesquisa" e armazena na variável "section".
     // Essa seção será usada para exibir os resultados da pesquisa.
     let section = document.getElementById("resultados-pesquisa");
-  
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+    if (!campoPesquisa){
+
+      section.innerHTML = "<p>Nenhum jogo encontrado.</p>";
+      return;
+    };
+
+    
     // Imprime no console a referência para a seção selecionada, útil para depuração.
-    console.log(section);
+    //console.log(section);
+    console.log(campoPesquisa);
+    
   
     // Inicializa uma string vazia para armazenar os resultados da pesquisa.
     // Essa string será preenchida com o HTML gerado para cada item encontrado.
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tag = "";
     // Itera sobre cada item (dado) no array "dados".
     // Assumindo que "dados" é um array de objetos com as propriedades: titulo, descricao, genero, plataformas, dataLancamento e link.
     for (let dado of dados) {
-      // Concatena o HTML para cada item à variável "resultados".
-      // Utiliza template literals (``) para facilitar a construção da string.
-      resultados += `
+
+      titulo = dado.titulo.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      tag = dado.tag.toLowerCase();
+
+       if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tag.includes(campoPesquisa) ){
+
+        // Concatena o HTML para cada item à variável "resultados".
+        // Utiliza template literals (``) para facilitar a construção da string.
+        resultados += `
         <div class="item-resultado">
           <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
           <p class="descricao-meta">${dado.descricao}</p>
@@ -24,9 +44,15 @@ function pesquisar() {
           <p class="descricao-meta ">Data de Lançamento: ${dado.dataLancamento}</p>
           <a href=${dado.link} target="_blank">Mais Informações</a>
         </div>
-      `;
+        `;
+      }
+
+
     }
-  
+    
+    if (!resultados){
+      resultados = "<p>Nada foi encontrado</p>"
+    }
     // Atribui o conteúdo HTML gerado à seção "resultados-pesquisa",
     // substituindo o conteúdo anterior.
     section.innerHTML = resultados;
--- data.js
+++ data.js
@@ -2,26 +2,29 @@ let dados = [
         {
             titulo: "Mullet Madjack",
             descricao: "Mullet Madjack é um jogo de tiro em primeira pessoa frenético que te coloca no meio de um anime clássico. Com visuais vibrantes e uma jogabilidade ágil, você controla Madjack, um protagonista cheio de estilo, em uma missão para salvar uma influenciadora famosa.",
-            genero: "Shooter",
+            genero: "Shooter, Ação",
             plataformas: ["PC", "PlayStation 4", "Xbox One", "Nintendo Switch"],
             dataLancamento: "2019-08-05",
-            link: "https://www.hammer95.com/"
+            link: "https://www.hammer95.com/",
+            tag:"mullet mad anime estilo"
         },
         {
             titulo: "Warhammer 40,000: Boltgun",
             descricao: "Mergulhe no universo sombrio de Warhammer 40,000 com este shooter em primeira pessoa clássico. Enfrente hordas de inimigos xenos e hereges enquanto você luta pela imperatriz.",
-            genero: "Shooter",
+            genero: "Shooter, Ação",
             plataformas: ["PC", "PlayStation 5", "Xbox Series X/S"],
             dataLancamento: "2022-03-24",
-            link: "https://store.steampowered.com/app/1919900/Warhammer_40000_Boltgun/"
+            link: "https://store.steampowered.com/app/1919900/Warhammer_40000_Boltgun/",
+            tag:"hammer war gun horda"
         },
         {
             titulo: "Cultic",
             descricao: "Cultic é um jogo de terror psicológico em primeira pessoa que te leva a uma jornada por uma ilha misteriosa. Desvende os segredos de um culto antigo e lute para sobreviver.",
-            genero: "Shooter",
+            genero: "Shooter, Terror",
             plataformas: ["PC", "PlayStation 4", "Xbox One", "Nintendo Switch"],
             dataLancamento: "2019-03-20",
-            link: "https://store.steampowered.com/app/1907410/Cultic/"
+            link: "https://store.steampowered.com/app/1907410/Cultic/",
+            tag:"culto psicologico terror"
         },
         {
             titulo: "Ion Fury",
@@ -29,31 +32,36 @@ let dados = [
             genero: "Shooter",
             plataformas: ["PC", "PlayStation 4", "Xbox One", "Nintendo Switch"],
             dataLancamento: "2019-08-05",
-            link: "https://store.steampowered.com/app/632370/Ion_Fury/"
+            link: "https://store.steampowered.com/app/632370/Ion_Fury/",
+            tag:"duke reboot nukem"
         },
         {
             titulo: "Prodeus",
             descricao: "Um shooter em primeira pessoa com gráficos pixel art 3D e trilha sonora metal, que combina a estética clássica com mecânicas modernas.",
-            genero: "Shooter",
+            genero: "Shooter ",
             plataformas: ["PC", "PlayStation 5", "Xbox Series X/S"],
             dataLancamento: "2022-03-24",
-            link: "https://store.steampowered.com/app/1372430/Prodeus/"
+            link: "https://store.steampowered.com/app/1372430/Prodeus/",
+            tag:"pixel art metal"
+
         },
         {
             titulo: "Amid Evil",
             descricao: "Um shooter lovecraftiano com armas poderosas e ambientes góticos. Uma homenagem aos clássicos dos anos 90.",
-            genero: "Shooter",
+            genero: "Shooter, Horror",
             plataformas: ["PC", "PlayStation 4", "Xbox One", "Nintendo Switch"],
             dataLancamento: "2019-03-20",
-            link: "https://store.steampowered.com/app/710140/Amid_Evil/"
+            link: "https://store.steampowered.com/app/710140/Amid_Evil/",
+            tag:"lovecraft cosmico gotico"
         },
         {
             titulo: "Dusk",
             descricao: "Um shooter retro que combina elementos de Doom e Quake, com uma atmosfera sombria e desafiadora.",
-            genero: "Shooter",
+            genero: "Shooter, Horror",
             plataformas: ["PC", "PlayStation 4", "Xbox One", "Nintendo Switch"],
             dataLancamento: "2018-10-10",
-            link: "https://store.steampowered.com/app/618620/Dusk/"
+            link: "https://store.steampowered.com/app/618620/Dusk/",
+            tag: "retro Doom Quake"
         }
 ];
 
